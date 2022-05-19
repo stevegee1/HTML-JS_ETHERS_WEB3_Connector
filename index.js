f2fc69ab7c68a2e1e76dec031d135942434cb799
stevegee1 <ogarstephen98@gmail.com>
2022-05-19 11:46:29 +0100

    Connecting web3 to html &Js frontend

---
+++ index.js
@@ -0,0 +1,116 @@
+const { ethers } = require("ethers");
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        ethereum.request({ method: 'eth_requestAccounts' });
+    }
+}
+async function execute() {
+    //contractAddress
+    //ABI
+    //A signer; any account that calls the function
+    //call the function
+    const contractAddress = "0x5fbdb2315678afecb367f032d93f642f64180aa3";
+    const abi = [
+        {
+            inputs: [
+                {
+                    internalType: "string",
+                    name: "_name",
+                    type: "string",
+                },
+                {
+                    internalType: "uint256",
+                    name: "_favoriteNumber",
+                    type: "uint256",
+                },
+            ],
+            name: "addPerson",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+        },
+        {
+            inputs: [
+                {
+                    internalType: "string",
+                    name: "",
+                    type: "string",
+                },
+            ],
+            name: "nameToFavoriteNumber",
+            outputs: [
+                {
+                    internalType: "uint256",
+                    name: "",
+                    type: "uint256",
+                },
+            ],
+            stateMutability: "view",
+            type: "function",
+        },
+        {
+            inputs: [
+                {
+                    internalType: "uint256",
+                    name: "",
+                    type: "uint256",
+                },
+            ],
+            name: "people",
+            outputs: [
+                {
+                    internalType: "uint256",
+                    name: "favoriteNumber",
+                    type: "uint256",
+                },
+                {
+                    internalType: "string",
+                    name: "name",
+                    type: "string",
+                },
+            ],
+            stateMutability: "view",
+            type: "function",
+        },
+        {
+            inputs: [],
+            name: "retrieve",
+            outputs: [
+                {
+                    internalType: "uint256",
+                    name: "",
+                    type: "uint256",
+                },
+            ],
+            stateMutability: "view",
+            type: "function",
+        },
+        {
+            inputs: [
+                {
+                    internalType: "uint256",
+                    name: "_favoriteNumber",
+                    type: "uint256",
+                },
+            ],
+            name: "store",
+            outputs: [],
+            stateMutability: "nonpayable",
+            type: "function",
+        },
+    ];
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    await contract.store(42);
+
+
+}
+
+module.exports = {
+    connect,
+    execute
+}
